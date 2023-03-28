38767b3a2a8dd191c0a48bd28542c7a10231c754
Deity2001 <98880618+Deity2001@users.noreply.github.com>
2023-03-28 04:18:42 -0700

    Add files via upload

---
+++ program12th.java
@@ -0,0 +1,12 @@
+class program12th {
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++){
+            System.out.print("*");
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+
+            }
+
+        }
+    }
+}
