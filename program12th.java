91d3714192865d9cb4da8f8daa0341b0c14f27a6
Deity2001 <98880618+Deity2001@users.noreply.github.com>
2023-03-28 21:20:55 -0700

    Update program12th.java

--- program12th.java
+++ program12th.java
@@ -1,6 +1,6 @@
 class program12th {
     public static void main(String[] args) {
-        for(int i=1;i<=7;i++){
+        for(int i=1;i<=5;i++){
             System.out.print("*");
             for(int j=1;j<=i;j++){
                 System.out.print(j);
